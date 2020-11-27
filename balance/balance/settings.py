fddc22ca03e0fec6d013e63ac91137b593a8ff14
Vishnu Varthan Rao <varthanv26018@gmail.com>
2020-11-27 10:27:15 +0530

    Update settings.py

--- balance/balance/settings.py
+++ balance/balance/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
